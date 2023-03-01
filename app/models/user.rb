70031490173b104fd2aa6604b1f4f0fe346ad6db
Vlad Datsenko <v_dats@protonmail.com>
2023-03-01 14:59:02 +0200

    authentication with a gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable,  :validatable
+        #  :rememberable,
   
   has_many :orders
 
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :first_name, :last_name, :email,  presence: true
 
 end
